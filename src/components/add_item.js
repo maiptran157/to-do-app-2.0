b86f46f01b42b5347e2b0ec492b49568ee12a5bc
Mia Tran <maiptran157@gmail.com>
2019-01-21 17:18:21 -0600

    create nav button to navigate back and forth to Add Item

--- src/components/add_item.js
+++ src/components/add_item.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import AddItemForm from './add_item_form';
+import { Link } from 'react-router-dom';
 
 class AddItem extends Component {
     render() {
         return (<div to="/add-item">
             <h1 className="center">Add Item</h1>
             <div className="row">
+                <Link className="s12" to="/">
+                    <button className="btn #4fc3f7 light-blue lighten-2">Item List</button>
+                </Link>
                 <div className="col-s8 offset-2">
                     <AddItemForm history={this.props.history} />
                 </div>
